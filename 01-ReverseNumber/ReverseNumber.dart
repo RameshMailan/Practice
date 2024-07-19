594e7be833cf6f33accfd576428b6c4140630b73
RameshMailan <justrameshm@gmail.com>
2024-07-19 13:02:13 +0530

    First proper push

--- 01-ReverseNumber/ReverseNumber.dart
+++ 01-ReverseNumber/ReverseNumber.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 void main(){
-    int num=192,divident,remainder;
+    int num, divident=0,remainder=0;
+    print("Enter a Number:");
+     num = int.parse(stdin.readLineSync()!);
     for(int i=1;i<=3;i++){
         divident=num % 10;
         remainder=remainder * 10+ divident;
